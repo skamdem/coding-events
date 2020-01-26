1bdd51273e2ece59cadf91bbb49a514cfeb234b5
sylvain kamdem <sylvain.kamdem@gmail.com>
2020-01-25 19:22:15 -0500

    end of lesson

---
+++ src/test/java/org/launchcade/codingevents/CodingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcade.codingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
